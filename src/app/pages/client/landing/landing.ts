a792d8149a05a072b6cbd4ca59fc58d8dc201ca6
Angel Diaz <agabrieldchavez@gmail.com>
2026-06-28 18:02:11 -0500

    fix/refactor de landing y correccion de las credenciales invalidas en el login

--- src/app/pages/client/landing/landing.ts
+++ src/app/pages/client/landing/landing.ts
@@ -1,43 +1,16 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { MarketStateService, Product } from '../../../services/market-state';
-import { DecimalPipe } from '@angular/common';
-import { CartService } from '../../../services/cart.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
-  imports: [RouterLink, DecimalPipe],
+  standalone: true,
   templateUrl: './landing.html',
   styleUrl: './landing.css'
 })
 export class Landing {
-  stateService = inject(MarketStateService);
-  cartService = inject(CartService);
-
-  get featuredProducts(): Product[] {
-    return this.stateService.products().filter(p => p.isVisible);
+  
+  // Método para el botón del Hero que baja suavemente a las "Features"
+  scrollToSection(element: HTMLElement): void {
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
-
-  get clientReviews() {
-    return this.stateService.reviews();
-  }
-
-  scrollToProducts(element: HTMLElement): void {
-    element.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  // Corregido: Un solo argumento para encajar perfectamente con tu landing.html
-  agregarAlCarrito(prod: any): void {
-    const productId = Number(prod.id);
-    const storeId = Number(prod.storeId || 1); // Fallback dinámico para evitar el TS2339
-
-    this.cartService.addToCartBackend(productId, storeId, 1).subscribe({
-      next: () => {
-        alert(`¡${prod.name} agregado al carrito en el servidor!`);
-      },
-      error: (err) => {
-        console.error('Error al agregar al carrito desde el landing:', err);
-      }
-    });
-  }
+  
 }
