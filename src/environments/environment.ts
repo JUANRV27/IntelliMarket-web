90499d8a798ff6cfbba325f036d0138701a77ab9
Juan José Rodríguez Velásquez <167146892+JUANRV27@users.noreply.github.com>
2026-07-03 22:46:09 -0500

    Update apiUrl to remove endpoint from production URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://intellimarket-api.onrender.com/api'
+  apiUrl: 'https://intellimarket-api.onrender.com/'
   //apiUrl: 'http:://localhost:8080/api'
 };
