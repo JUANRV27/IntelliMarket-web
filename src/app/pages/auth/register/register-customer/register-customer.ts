e23489d0908f3eb6fb3ee0dd0727475234306499
MauColab <mauriciocanchisf@gmail.com>
2026-07-04 19:38:07 -0500

    fix: Modales para credenciales erróneas e información inválida la crear cuenta para vendedor y cliente.

--- src/app/pages/auth/register/register-customer/register-customer.ts
+++ src/app/pages/auth/register/register-customer/register-customer.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../../services/auth.service';
@@ -23,8 +23,38 @@ export class RegisterCustomer {
   password = '';
   errorMessage = '';
 
+  // Modal de error
+  showModal = signal(false)
+  modalErrors = signal<string[]>([])
+
+  
+
   onRegister() {
     this.errorMessage = '';
+
+    // Armamos el paquete declarando explícitamente que es un SELLER
+    const errors: string[] = [];
+
+    // VALIDACIÓN CLIENTE: Alimentamos el modal antes de ir al servidor
+    if (!this.firstName.trim()) errors.push('El nombre del representante es obligatorio.');
+    if (!this.lastName.trim()) errors.push('Los apellidos del representante son obligatorios.');
+    
+    if (!this.email.includes('@')) {
+      errors.push("El correo electrónico debe contener un '@'.");
+    }
+    if (this.email.length > 30) {
+      errors.push('El correo electrónico no debe superar los 30 caracteres.');
+    }
+    if (this.password.length < 8) {
+      errors.push('La contraseña debe tener al menos 8 caracteres.');
+    }
+
+    // Si existen inconsistencias locales, abrimos el modal y frenamos la petición
+    if (errors.length > 0) {
+      this.modalErrors.set(errors);
+      this.showModal.set(true);
+      return;
+    }
     
     // Armamos el paquete exacto para Spring Boot
     const payload = {
@@ -42,10 +72,21 @@ export class RegisterCustomer {
         this.router.navigate(['/auth/login']); // Te redirige al login
       },
       error: (err) => {
-        console.error('Error del backend:', err);
+        //console.error('Error del backend:', err);
         // Si el backend manda un mensaje específico, lo mostramos
-        this.errorMessage = err.error?.message || 'Error al registrar. Verifica los datos o si el correo ya existe.';
+        //this.errorMessage = err.error?.message || 'Error al registrar. Verifica los datos o si el correo ya existe.';
+        console.error('Error al registrar cliente:', err);
+        const backendMessage = err.error?.message || 'Error al registrar el cliente. Verifica los datos.';
+        
+        this.modalErrors.set([backendMessage]);
+        this.showModal.set(true);
       }
     });
   }
+
+  cerrarModal() {
+    this.showModal.set(false);
+    this.modalErrors.set([]);
+  }
 }
+
